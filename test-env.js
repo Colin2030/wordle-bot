bb1c7f81e2efc4efea904bfaf6e0e6d89442a169
Colin2030 <colin2030@gmail.com>
2025-05-01 10:09:55 +0100

    Debug: environment test script

---
+++ test-env.js
@@ -0,0 +1,3 @@
+// test-env.js
+console.log("🔎 TELEGRAM_BOT_TOKEN:", process.env.TELEGRAM_BOT_TOKEN ? "SET" : "NOT SET");
+console.log("🔎 GOOGLE_CREDENTIALS_B64:", process.env.GOOGLE_CREDENTIALS_B64 ? "SET" : "NOT SET");
