d22d18b0c5bcea83487c0c82e633a519f4aa5033
Colin2030 <129393734+Colin2030@users.noreply.github.com>
2025-05-02 11:58:09 +0100

    giphymemes

---
+++ giphyMemes_snippet.js
@@ -0,0 +1,18 @@
+
+const memes = [
+  "https://media.giphy.com/media/l0MYGBRPLx7xO8zYs/giphy.gif",
+  "https://media.giphy.com/media/3o7qE1YN7aBOFPRw8E/giphy.gif",
+  "https://media.giphy.com/media/xT9IgIc0lryrxvqVGM/giphy.gif",
+  "https://media.giphy.com/media/3ohzdIuqJoo8QdKlnW/giphy.gif",
+  "https://media.giphy.com/media/l3q2K5jinAlChoCLS/giphy.gif"
+];
+
+const victoryMemes = [
+  "https://media.giphy.com/media/26n6WywJyh39n1pBu/giphy.gif",
+  "https://media.giphy.com/media/xT5LMzIK1AdZJavbTG/giphy.gif",
+  "https://media.giphy.com/media/111ebonMs90YLu/giphy.gif",
+  "https://media.giphy.com/media/xUOwFf3hdB9JxSPS1a/giphy.gif",
+  "https://media.giphy.com/media/fxsqOYnIMEefC/giphy.gif"
+];
+
+module.exports = { memes, victoryMemes };
