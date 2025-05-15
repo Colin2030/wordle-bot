9b90ca4624c91ce252a2620437741d00df752510
Colin2030 <129393734+Colin2030@users.noreply.github.com>
2025-05-15 11:17:57 +0100

    Create playerProfiles.js

---
+++ playerProfiles.js
@@ -0,0 +1,42 @@
+module.exports = {
+  "Tobi": {
+    "pronoun": "she",
+    "possessive": "her"
+  },
+  "Colin": {
+    "pronoun": "he",
+    "possessive": "his"
+  },
+  "Alex": {
+    "pronoun": "they",
+    "possessive": "their"
+  },
+  "Sam": {
+    "pronoun": "they",
+    "possessive": "their"
+  },
+  "Jess": {
+    "pronoun": "she",
+    "possessive": "her"
+  },
+  "Jamie": {
+    "pronoun": "he",
+    "possessive": "his"
+  },
+  "Chris": {
+    "pronoun": "he",
+    "possessive": "his"
+  },
+  "Pat": {
+    "pronoun": "they",
+    "possessive": "their"
+  },
+  "Jordan": {
+    "pronoun": "they",
+    "possessive": "their"
+  },
+  "Taylor": {
+    "pronoun": "they",
+    "possessive": "their"
+  }
+};
